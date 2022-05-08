f804f7b9967e635f4bd207890c9ea15dc5fdda25
jmnc2 <49760581+jmnc2@users.noreply.github.com>
2022-05-08 14:43:42 +0100

    s

--- src/groups.cpp
+++ src/groups.cpp
@@ -4,7 +4,7 @@
 
 byte setBits(byte numBytes, byte bitx1, byte bitx2, byte bitx3, byte bitx4, byte bitx5, byte bitx6, byte bitx7) {
   byte i;
-  i=0;
+  i = 0;
   if (bitx1 > 0) {
     bitSet(i, bitx1 - 1);// the first bit is 0, so sub 1. Therefore bitx1 = 1 will set the least-significant (rightmost) bit.
     if (bitx2 > 0) {
@@ -29,48 +29,77 @@ byte setBits(byte numBytes, byte bitx1, byte bitx2, byte bitx3, byte bitx4, byte
   return i;
 }
 
-  byte Groups::getGroup(byte i, char r[ ]) {
-    if (i <= numOfSmallGroups) {
-      blockObj3.readSlot(i, r);
-      return 3;
-    } else if (i <= numOfGroups) {
-      blockObj10.readSlot(i - numOfSmallGroups, r);
-      return 10;
+byte Groups::getGroup(byte i, char r[ ]) {
+  if (i <= numOfSmallGroups) {
+    blockObj3.readSlot(i, r);
+    return 3;
+  } else if (i <= numOfGroups) {
+    blockObj10.readSlot(i - numOfSmallGroups, r);
+    return 10;
+  }
+  // else out of range.
+  return 0;
+}
+
+/**
+ * @brief Store a group to storage i.e. EEPROM
+ * @details This will store the group to storage. To position determins the size in bytes of the storage.
+ *
+ *
+ * @param i The index in storage to store the group in.
+ * @param r The array of bytes passed in. first element at 0 should be the size of the array.
+ * @return byte
+ */
+byte Groups::setGroup(byte i, char r[ ]) {
+  byte l, x;
+  if (i <= numOfSmallGroups) {
+    l = blockObj3.blockSize();
+    if (r[0] == l) {
+      blockObj3.writeSlot(i, r + 1);
+    } else {
+      if (l > r[0]) l = r[0];
+      for (x = 0; x < l; x++) {
+        blockObj3.writeByte(i, x, r[x + 1]);
+      }
     }
-    // else out of range.
-    return 0;
-  }
-
-  byte Groups::setGroup(byte i, char r[ ]) {
-    if (i <= numOfSmallGroups) {
-      blockObj3.writeSlot(i, r);
-      return 3;
-    } else if (i <= numOfGroups) {
-      blockObj10.writeSlot(i - numOfSmallGroups, r);
-      return 10;
+    return 3;
+  } else if (i <= numOfGroups) {
+    l = blockObj10.blockSize();
+    i -=numOfSmallGroups;
+    if (r[0] == l) {
+      blockObj10.writeSlot(i, r + 1);
+    } else {
+      if (l > r[0]) l = r[0];
+      for (x = 0; x < l; x++) {
+        blockObj10.writeByte(i, x, r[x + 1]);
+      }
     }
-    // else out of range.
-    return 0;
+
+    blockObj10.writeSlot(i, r);
+    return 10;
   }
+  // else out of range.
+  return 0;
+}
 
-  void Groups::initStuff(storageBlock_C & o, byte id1, storageBlock_C & o2, byte id2) {
-    blockObj3.setMemManager(o);
-    blockObj3.setId(id1);
-    blockObj10.setMemManager(o2);
-    blockObj10.setId(id2);
-    numOfSmallGroups = blockObj3.getSlots();
-    numOfGroups = numOfSmallGroups + blockObj10.getSlots();
-  }
+void Groups::initStuff(storageBlock_C& o, byte id1, storageBlock_C& o2, byte id2) {
+  blockObj3.setMemManager(o);
+  blockObj3.setId(id1);
+  blockObj10.setMemManager(o2);
+  blockObj10.setId(id2);
+  numOfSmallGroups = blockObj3.getSlots();
+  numOfGroups = numOfSmallGroups + blockObj10.getSlots();
+}
 
-  void Groups::printInfo() {
-    //byte i;
-    io_printHeading("Printing setup");
-    //io_print( "Storeage id" ); io_print_n( blockObj.getId() ); 
-    io_print("Max groups"); io_println_n(numOfSmallGroups + numOfSmallGroups);// io_print( ", Lights" ); io_println_n( numLights );
+void Groups::printInfo() {
+  //byte i;
+  io_printHeading("Printing setup");
+  //io_print( "Storeage id" ); io_print_n( blockObj.getId() ); 
+  io_print("Max groups"); io_println_n(numOfSmallGroups + numOfSmallGroups);// io_print( ", Lights" ); io_println_n( numLights );
 
-    // //io_print("Size of room record extened(room_TE) : ");  io_println_n(roomsRecSizeE);
-    // for ( i = 0; i < maxLights; i++ ) {
-    //   printLightInfo( i );
-    // }
+  // //io_print("Size of room record extened(room_TE) : ");  io_println_n(roomsRecSizeE);
+  // for ( i = 0; i < maxLights; i++ ) {
+  //   printLightInfo( i );
+  // }
 
-  }
+}
